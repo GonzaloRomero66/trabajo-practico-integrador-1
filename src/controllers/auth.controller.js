2847ad4e36e65736ea9e7ca1b138a03e6ca1a00b
GonzaloRomero66 <estudio.romero66@gmail.com>
2026-09-06 23:08:08 -0300

    se agrego el inicio de sesion de los usuarios

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -38,3 +38,45 @@ export const register = async (req, res) => {
         });
     }
 };
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await UserModel.findOne({
+            where: { email }
+        });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Email o contraseña incorrectos"
+            });
+        }
+
+        const passwordCorrecta = await comparePassword(password, user.password);
+
+        if (!passwordCorrecta) {
+            return res.status(401).json({
+                message: "Email o contraseña incorrectos"
+            });
+        }
+
+        const token = generateToken({
+            id: user.id,
+            role: user.role
+        });
+
+        res.cookie("token", token, {
+            httpOnly: true
+        });
+
+        return res.status(200).json({
+            message: "Inicio de sesión correcto"
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error al iniciar sesión",
+            error: error.message
+        });
+    }
+};
