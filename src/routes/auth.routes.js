730469464ac194fbd75b72f83f0b71f238f5043f
GonzaloRomero66 <estudio.romero66@gmail.com>
2026-09-07 00:51:06 -0300

    se hizo las rutas para registrarse, iniciar sesion, cerrar sesion y lo demas

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+import {
+    register,
+    login,
+    getProfile,
+    updateProfile,
+    logout
+} from "../controllers/auth.controller.js";
+
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+
+const router = Router();
+
+router.post("/register", register);
+
+router.post("/login", login);
+
+router.get("/profile", authMiddleware, getProfile);
+
+router.put("/profile", authMiddleware, updateProfile);
+
+router.post("/logout", logout);
+
+export default router;
