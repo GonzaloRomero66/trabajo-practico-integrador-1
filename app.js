1b473c13fcebd4a0f1ad6bb02c72ae4b906a7d3d
GonzaloRomero66 <estudio.romero66@gmail.com>
2026-09-07 01:46:00 -0300

    se conectaron las rutas al app.js

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv'
 import { sequelize } from './src/config/database.js'
 import cookieParser from "cookie-parser";
 import authRoutes from "./src/routes/auth.routes.js"
+import userRoutes from "./src/routes/user.routes.js";
+import articleRoutes from "./src/routes/article.routes.js";
+import tagRoutes from "./src/routes/tag.routes.js";
+import articleTagRoutes from "./src/routes/articleTag.routes.js";
 import "./src/models/relations.js";
 dotenv.config()
 const app = express()
@@ -10,6 +14,10 @@ app.use(express.json())
 app.use(cookieParser());
 
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/articles", articleRoutes);
+app.use("/tags", tagRoutes);
+app.use("/articles-tags", articleTagRoutes);
 
 console.log(process.env.PORT)
 const port = process.env.PORT
