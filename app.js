371712ee23862e47211606ec3a4c5f3c97b7443b
GonzaloRomero66 <estudio.romero66@gmail.com>
2026-09-07 00:55:07 -0300

    se importo las rutas de auth en app

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ import express  from 'express'
 import dotenv from 'dotenv'
 import { sequelize } from './src/config/database.js'
 import cookieParser from "cookie-parser";
-
+import authRoutes from "./src/routes/auth.routes.js"
 dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cookieParser());
 
+app.use("/auth", authRoutes);
 
 console.log(process.env.PORT)
 const port = process.env.PORT
