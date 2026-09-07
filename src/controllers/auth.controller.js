8fca935c189f9b6cfe8d7401749babf05ec77941
GonzaloRomero66 <estudio.romero66@gmail.com>
2026-09-06 23:42:04 -0300

    se creo logout para que se cierre sesion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -144,3 +144,18 @@ export const updateProfile = async (req, res) => {
         });
     }
 };
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token");
+
+        return res.status(200).json({
+            message: "Sesión cerrada correctamente"
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error al cerrar sesión",
+            error: error.message
+        });
+    }
+};
